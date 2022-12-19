7729df58971a0b65b6a7538d9326cb99eccff2bb
unkel-kp <kp07iam@gmail.com>
2022-12-20 02:32:33 +0530

    Home content

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div>
+        <h1> This is HOME! </h1>
+      </div>
       
     </>
   )
